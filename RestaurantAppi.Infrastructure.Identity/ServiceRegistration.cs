a59a6861e7e9a1cc64faa6bc5ce03852b03a758c
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2023-05-23 22:04:07 -0400

    Fix: Problem with connection

--- RestaurantAppi.Infrastructure.Identity/ServiceRegistration.cs
+++ RestaurantAppi.Infrastructure.Identity/ServiceRegistration.cs
@@ -24,10 +24,10 @@ namespace RestaurantAppi.Infrastructure.Identity
         public static void AddIdentityInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
 			var connection = configuration.GetConnectionString("PostgreSQL");
-			var password = Environment.GetEnvironmentVariable("PassCockroachDB");
-			var host = Environment.GetEnvironmentVariable("HostCockroachDB");
-			connection = connection.Replace("#", password);
-            connection = connection.Replace("ServerHost", host);
+			//var password = Environment.GetEnvironmentVariable("PassCockroachDB");
+			//var host = Environment.GetEnvironmentVariable("HostCockroachDB");
+			//connection = connection.Replace("#", password);
+            //connection = connection.Replace("ServerHost", host);
 			#region Contexts
 			if (configuration.GetValue<bool>("UseInMemoryDatabase"))
             {
